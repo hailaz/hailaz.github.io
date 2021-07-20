8edd6467846ba52be70df9e64a4c251715eeb7d8
hailaz <739476267@qq.com>
2021-07-20 14:04:03 +0800

    update

---
+++ static/index.js
@@ -0,0 +1,23 @@
+// 设置树数据
+
+v.treeData = [{
+    label: '电子产品',
+    children: [{
+        label: '手机',
+        children: [{
+            label: '小米',
+            children: [{
+                label: 'mi10',
+                link: "http://www.baidu.com"
+            }, {
+                label: 'mi11 - 「小白」小米11全面测评：最贵屏幕+骁龙888有多顶？',
+                link: "https://www.bilibili.com/video/BV1jo4y1Z7jb"
+            }]
+        }]
+    }]
+}, {
+    label: '日用',
+    children: [{
+        label: '椅子'
+    }]
+}]
