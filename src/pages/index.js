b22f1c9a3d8642109fd8ecb398c5b1cfc8359e21
hailaz <739476267@qq.com>
2025-02-10 17:42:45 +0800

    fix: index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import styles from './index.module.css';
 
@@ -18,6 +18,25 @@ function HomepageHeader() {
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
+  const googleInputRef = useRef(null);
+  const baiduInputRef = useRef(null);
+  const [googleInput, setGoogleInput] = useState('');
+  const [baiduInput, setBaiduInput] = useState('');
+
+  const clearInput = (inputRef, setInput) => {
+    if (inputRef.current) {
+      inputRef.current.value = '';
+      inputRef.current.focus();
+      setInput('');
+    }
+  };
+
+  const handleKeyDown = (e, inputRef, setInput) => {
+    if (e.key === 'Delete') {
+      clearInput(inputRef, setInput);
+    }
+  };
+
   return (
     <Layout
       title={`欢迎来到 ${siteConfig.title}`}
@@ -27,11 +46,44 @@ export default function Home() {
         <div className={clsx(styles.pageWrapper)}>
           <div className={clsx(styles.searchContainer)}>
             <form action="https://www.google.com/search" method="GET" target="_blank" className={styles.searchForm}>
-              <input type="text" name="q" size="50" className={styles.searchInput} />
+              <input 
+                ref={googleInputRef}
+                type="text" 
+                name="q" 
+                size="50" 
+                className={styles.searchInput} 
+                autoFocus
+                onKeyDown={(e) => handleKeyDown(e, googleInputRef, setGoogleInput)}
+                onChange={(e) => setGoogleInput(e.target.value)}
+                value={googleInput}
+              />
+              {googleInput && (
+                <button 
+                  type="button" 
+                  onClick={() => clearInput(googleInputRef, setGoogleInput)} 
+                  className={styles.clearButton}
+                >×</button>
+              )}
               <input type="submit" value="google" className={styles.searchButton} />
             </form>
             <form action="https://www.baidu.com/s" method="GET" target="_blank" className={styles.searchForm}>
-              <input type="text" name="wd" size="50" className={styles.searchInput} />
+              <input 
+                ref={baiduInputRef}
+                type="text" 
+                name="wd" 
+                size="50" 
+                className={styles.searchInput}
+                onKeyDown={(e) => handleKeyDown(e, baiduInputRef, setBaiduInput)}
+                onChange={(e) => setBaiduInput(e.target.value)}
+                value={baiduInput}
+              />
+              {baiduInput && (
+                <button 
+                  type="button" 
+                  onClick={() => clearInput(baiduInputRef, setBaiduInput)} 
+                  className={styles.clearButton}
+                >×</button>
+              )}
               <input type="submit" value="百度一下" className={styles.searchButton} />
             </form>
           </div>
