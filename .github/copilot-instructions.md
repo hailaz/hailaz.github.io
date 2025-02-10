f1feedead42eef320d0f632c16425035ee5f64a3
hailaz <739476267@qq.com>
2025-02-10 17:12:50 +0800

    css: 首页改版

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,44 @@
+# 项目开发指南
+
+## 项目概述
+- 这是一个基于 Docusaurus 的个人技术文档网站
+- 主要用于技术文章分享和知识积累
+- 使用 Markdown 和 MDX 格式编写文档
+
+## 开发规范
+
+### 文件组织
+- 文档放置在 `docs` 目录下
+- 博客文章放置在 `blog` 目录下
+- 静态资源存放在 `static` 目录
+- 组件放置在 `src/components` 目录
+
+### 命名规范
+- 文档文件使用有意义的英文名称，以 `.md` 或 `.mdx` 结尾
+- 博客文件名格式：`YYYY-MM-DD-title.md`
+- React 组件使用 PascalCase 命名法
+- CSS 模块文件使用 `*.module.css` 命名
+
+### 文档编写规范
+- 文档必须包含 front matter
+- 使用 Markdown 和 MDX 语法
+- 代码块需指定语言类型
+- 图片需添加 alt 文本描述
+- 保持清晰的文章结构和段落划分
+
+### Git 规范
+- commit 信息需简明扼要
+- 使用英文书写 commit 信息
+- branch 命名规范：feature/xxx, fix/xxx
+
+## 本地开发
+1. 克隆仓库
+2. 安装依赖：`npm install`
+3. 本地预览：`npm start`
+4. 构建网站：`npm run build`
+
+## 部署说明
+- 提交到 master 分支自动触发部署
+- 通过 GitHub Actions 自动构建
+- 构建输出目录为 `build`
+- 确保提交前本地构建无误
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,78 +28,53 @@ export default function Home() {
           <div className={clsx(styles.mysearch)}>
             <form action="https://www.google.com/search" method="GET" target="_blank">
               <input type="text" name="q" size="50" />
-              <input type="submit" value="google" style={{ width: '80px' }} />
+              <input type="submit" value="google" />
             </form>
             <form action="https://www.baidu.com/s" method="GET" target="_blank">
               <input type="text" name="wd" size="50" />
-              <input type="submit" value="百度一下" style={{ width: '80px' }} />
+              <input type="submit" value="百度一下" />
             </form>
           </div>
           {/* <HomepageFeatures /> */}
           <div className={clsx(styles.mytable)}>
-            <table>
-              <tbody>
-                <tr>
-                  <td>常用</td>
-                  <td>
-                    <a href='https://t.bilibili.com/' target="_blank">B站</a>
-                  </td>
-                  <td>
-                    <a href='https://www.speedtest.cn/' target="_blank">测速</a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>购物</td>
-                  <td>
-                    <a href='http://www.taobao.com/' target="_blank">淘宝</a>
-                  </td>
-                  <td>
-                    <a href='http://www.xiaomi.com/' target="_blank">小米</a>
-                  </td>
-                  <td>
-                    <a href='http://www.jd.com/' target="_blank">京东</a>
-                  </td>
-                  <td>
-                    <a href='http://www.amazon.cn/' target="_blank">亚马逊</a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>影视</td>
-                  <td>
-                    <a href='http://www.youku.com/' target="_blank">优酷</a>
-                  </td>
-                  <td>
-                    <a href='http://lpl.qq.com/' target="_blank">LPL</a>
-                  </td>
-                  <td>
-                    <a href='http://www.iqiyi.com/' target="_blank">爱奇艺</a>
-                  </td>
-                  <td>
-                    <a href='http://v.qq.com/' target="_blank">腾讯视频</a>
-                  </td>
-                  <td>
-                    <a href='https://www.mgtv.com/' target="_blank">芒果TV</a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>社区</td>
-                  <td>
-                    <a href='http://www.facebook.com/' target="_blank">facebook</a>
-                  </td>
-                  <td>
-                    <a href='https://www.google.com/' target="_blank">
-                      <img src="https://www.example.com/google-icon.png" alt="Google"/>
-                    </a>
-                  </td>
-                  <td>
-                    <a href='https://www.youtube.com/' target="_blank">youtube</a>
-                  </td>
-                  <td>
-                    <a href='https://twitter.com/' target="_blank">twitter</a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <div className={styles.linkSection}>
+              <div className={styles.sectionTitle}>常用</div>
+              <div className={styles.linkList}>
+                <a className={styles.linkItem} href='https://t.bilibili.com/' target="_blank">B站</a>
+                <a className={styles.linkItem} href='https://www.speedtest.cn/' target="_blank">测速</a>
+              </div>
+            </div>
+
+            <div className={styles.linkSection}>
+              <div className={styles.sectionTitle}>购物</div>
+              <div className={styles.linkList}>
+                <a className={styles.linkItem} href='http://www.taobao.com/' target="_blank">淘宝</a>
+                <a className={styles.linkItem} href='http://www.xiaomi.com/' target="_blank">小米</a>
+                <a className={styles.linkItem} href='http://www.jd.com/' target="_blank">京东</a>
+                <a className={styles.linkItem} href='http://www.amazon.cn/' target="_blank">亚马逊</a>
+              </div>
+            </div>
+
+            <div className={styles.linkSection}>
+              <div className={styles.sectionTitle}>影视</div>
+              <div className={styles.linkList}>
+                <a className={styles.linkItem} href='http://www.youku.com/' target="_blank">优酷</a>
+                <a className={styles.linkItem} href='http://lpl.qq.com/' target="_blank">LPL</a>
+                <a className={styles.linkItem} href='http://www.iqiyi.com/' target="_blank">爱奇艺</a>
+                <a className={styles.linkItem} href='http://v.qq.com/' target="_blank">腾讯视频</a>
+                <a className={styles.linkItem} href='https://www.mgtv.com/' target="_blank">芒果TV</a>
+              </div>
+            </div>
+
+            <div className={styles.linkSection}>
+              <div className={styles.sectionTitle}>社区</div>
+              <div className={styles.linkList}>
+                <a className={styles.linkItem} href='http://www.facebook.com/' target="_blank">facebook</a>
+                <a className={styles.linkItem} href='https://www.google.com/' target="_blank">Google</a>
+                <a className={styles.linkItem} href='https://www.youtube.com/' target="_blank">youtube</a>
+                <a className={styles.linkItem} href='https://twitter.com/' target="_blank">twitter</a>
+              </div>
+            </div>
           </div>
         </div>
 
